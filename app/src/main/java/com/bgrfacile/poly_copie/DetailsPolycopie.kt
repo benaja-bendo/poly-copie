742301389e5d4d53c2e02d9095833ac74b450ed8
benji_mac <benaja.bendo02@gmail.com>
2024-07-26 01:02:55 +0200

    wip design 01 DetailsPolycopie

--- app/src/main/java/com/bgrfacile/poly_copie/DetailsPolycopie.kt
+++ app/src/main/java/com/bgrfacile/poly_copie/DetailsPolycopie.kt
@@ -1,34 +1,67 @@
 package com.bgrfacile.poly_copie
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DetailsPolycopie(itemId: String, paddingValues: PaddingValues) {
-
     val item = itemId.toIntOrNull()?.let { getFakePolycopieById(it) }
+
     Column(
         modifier = Modifier
             .fillMaxHeight()
             .padding(paddingValues)
     ) {
-        if (item != null) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp),
-                onClick = { /*TODO*/ }
+        item?.let {
+            Text(
+                text = it.name,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                Text(text = item.name)
+                items(it.imageResources.size) { index ->
+                    val imageResource = it.imageResources[index]
+                    ImageCard(imageResource)
+                }
             }
         }
+    }
+}
+
+@Composable
+fun ImageCard(imageResource: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .clickable { /* TODO: Action on image click */ }
+    ) {
+        val painter: Painter = painterResource(id = imageResource)
+        Image(
+            painter = painter,
+            contentDescription = null, // Provide a description for accessibility
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
     }
 }
--- app/src/main/java/com/bgrfacile/poly_copie/Polycopie.kt
+++ app/src/main/java/com/bgrfacile/poly_copie/Polycopie.kt
@@ -9,6 +9,7 @@ data class Polycopie(
     val id: Int,
     val name: String,
     val description: String,
+    val imageResources: List<Int>,
     val createdAt: Date,
 )
 
@@ -16,16 +17,60 @@ data class Polycopie(
 @RequiresApi(Build.VERSION_CODES.O)
 fun getFakePolycopie(): List<Polycopie> {
     return listOf(
-        Polycopie(1, "Polycopie 1", "Polycopie 1", Date.from(Instant.now())),
-        Polycopie(2, "Polycopie 2", "Polycopie 2", Date.from(Instant.now())),
-        Polycopie(3, "Polycopie 3", "Polycopie 3", Date.from(Instant.now())),
-        Polycopie(4, "Polycopie 4", "Polycopie 4", Date.from(Instant.now())),
-        Polycopie(5, "Polycopie 5", "Polycopie 5", Date.from(Instant.now())),
-        Polycopie(6, "Polycopie 6", "Polycopie 6", Date.from(Instant.now())),
-        Polycopie(7, "Polycopie 7", "Polycopie 7", Date.from(Instant.now())),
-        Polycopie(8, "Polycopie 8", "Polycopie 8", Date.from(Instant.now())),
-        Polycopie(9, "Polycopie 9", "Polycopie 9", Date.from(Instant.now())),
-        Polycopie(10, "Polycopie 10", "Polycopie 10", Date.from(Instant.now())),
+        Polycopie(
+            1,
+            "Polycopie 1",
+            "Polycopie 1",
+            listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ),
+            Date.from(Instant.now()),
+        ),
+        Polycopie(
+            2, "Polycopie 2", "Polycopie 2", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            3, "Polycopie 3", "Polycopie 3", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            4, "Polycopie 4", "Polycopie 4", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            5, "Polycopie 5", "Polycopie 5", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            6, "Polycopie 6", "Polycopie 6", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            7, "Polycopie 7", "Polycopie 7", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            8, "Polycopie 8", "Polycopie 8", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            9, "Polycopie 9", "Polycopie 9", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
+        Polycopie(
+            10, "Polycopie 10", "Polycopie 10", listOf(
+                R.drawable.image1, R.drawable.image1, R.drawable.image1
+            ), Date.from(Instant.now())
+        ),
     )
 }
 
